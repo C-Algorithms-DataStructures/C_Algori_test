280d2d71fe57e7a5a0f4ac8bbab12e4c3873db58
x1184 <x118422835@163.com>
2018-05-12 14:26:04 +0800

    修正字符串问题

--- 字符串翻转.c
+++ 字符串翻转.c
@@ -1,9 +1,10 @@
-/*ʵ���ַ�����ת*/  
+/*实现字符串翻转*/  
 #include<stdio.h>
 
 void main(){
 char* reverse_str(char* str)  
 {  
+    if(NULL == str) //字符串为空直接返回  
     {  
         return str;  
     }  
@@ -11,14 +12,14 @@ char* reverse_str(char* str)
     char *end;  
     begin = end = str;  
   
-    while(*end != '\0') //endָ���ַ�����ĩβ  
+    while(*end != '\0') //end指向字符串的末尾  
     {  
         end++;  
     }  
     --end;  
   
     char temp;  
-    while(begin < end) //���������ַ�  
+    while(begin < end) //交换两个字符  
     {  
         temp = *begin;  
         *begin = *end;  
@@ -27,6 +28,6 @@ char* reverse_str(char* str)
         end--;  
     }  
   
-    return str; //���ؽ��  
+    return str; //返回结果  
 }  
 }
