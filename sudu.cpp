e0ac19ae9503255636f5457a9ebee66586eeeadc
zerofivetwoone <39164106+zerofivetwoone@users.noreply.github.com>
2018-05-15 18:11:37 +0800

    money
    存钱  逆推

---
+++ sudu.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	int i,j;
+	for(i=95860;i<100000;i++){
+		if(i%10==i/10000){
+			if((i%100)/10==(i/1000)%10){printf("%d\n"£¬i);}
+			else continue;
+		}
+		else continue;
+	}
+}
