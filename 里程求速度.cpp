1625ba7dcc0fdac4425856842adf8e5200f7cf13
zerofivetwoone <39164106+zerofivetwoone@users.noreply.github.com>
2018-05-15 18:22:18 +0800

    Rename sudu.cpp to 里程求速度.cpp

--- 里程求速度.cpp
+++ 里程求速度.cpp
@@ -4,7 +4,7 @@ main(){
 	int i,j;
 	for(i=95860;i<100000;i++){
 		if(i%10==i/10000){
-			if((i%100)/10==(i/1000)%10){printf("%d\n"£¬i);}
+			if((i%100)/10==(i/1000)%10){printf("%d\n"Â£Â¬i);}
 			else continue;
 		}
 		else continue;
