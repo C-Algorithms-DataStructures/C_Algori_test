9b3112dba09b3ec6c28a3acc0da47dfc3cb69ea8
zerofivetwoone <39164106+zerofivetwoone@users.noreply.github.com>
2018-05-17 16:46:06 +0800

    里程求速度
    确定下一个车程求速度

--- 里程求速度.cpp
+++ 里程求速度.cpp
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<conio.h>
 main(){
-	int i,j;
+	int i;
+	float j;
 	for(i=95860;i<100000;i++){
 		if(i%10==i/10000){
-			if((i%100)/10==(i/1000)%10){printf("%d\n",i);}
+			if((i%100)/10==(i/1000)%10){printf("next number:%d\n",i);break;}
 			else continue;
 		}
 		else continue;
 	}
+	j=i;
+	j=(j-95859)/2;
+	printf("speed:%f\n",j);
+	return 0;
 }
