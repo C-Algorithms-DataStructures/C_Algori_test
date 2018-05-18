eb19ebbfebf52a295ff2950fccc1bfbb18db90af
zerofivetwoone <39164106+zerofivetwoone@users.noreply.github.com>
2018-05-18 21:50:42 +0800

    用函数求平均值
    两步函数，可以随时二合一改进

---
+++ 平均值-函数.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+float sum1(float a[]){
+	int i;
+	float sum=a[0];
+	for(i=1;i<10;i++)
+	     sum+=a[i];
+	return sum;
+}
+float average1(float sum){ 
+   float average;
+	average= sum/5;
+	return average;
+}
+void main(){
+	float score[10],average,sum;
+	int i;
+	printf("please input 10 number:");
+	for(i=0;i<10;i++){scanf("%f",&score[i]);}
+	sum=sum1(score);
+	average=average1(sum);	
+	printf("sum=%.2f,average=%.2f\n",sum,average);
+	getch();
+}
+
